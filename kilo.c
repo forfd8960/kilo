4f43cca934ccac36c0d9146f0d120ae9677f0270
forfd8960 <forfd8960@gmail.com>
2024-03-11 22:34:46 +0800

    Error Handling

--- kilo.c
+++ kilo.c
@@ -1,19 +1,35 @@
+/*** includes ***/
+
+#include <ctype.h>
+#include <errno.h>
 #include <termios.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
 
+/*** data ***/
+
 struct termios orig_termios;
+
+/*** terminal ***/
+
+void printAndExit(const char *s)
+{
+    perror(s);
+    exit(1);
+}
 
 void disableRawMode()
 {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+        printAndExit("tcsetattr");
 }
 
 void enableRawMode()
 {
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+        printAndExit("tcgetattr");
     atexit(disableRawMode);
 
     struct termios raw = orig_termios;
@@ -26,17 +42,22 @@ void enableRawMode()
     raw.c_cc[VMIN] = 0;  // set to 0, so that read returns as soon as there is any input to be read.
     raw.c_cc[VTIME] = 1; // set 1/10 seconds, 100 milliseconds.
 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == 1)
+        printAndExit("tcsetattr");
 }
+
+/*** init ***/
 
 int main()
 {
+
     enableRawMode();
 
-    char c = '\0';
-    read(STDIN_FILENO, &c, 1);
     while (1)
     {
+        char c = '\0';
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
+            printAndExit("read");
         if (iscntrl(c))
         {
             printf("%d\r\n", c);
