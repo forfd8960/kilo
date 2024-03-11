1ec83075202beac366b9082ea53bac23ce0c1e8a
forfd8960 <forfd8960@gmail.com>
2024-03-11 09:12:32 +0800

    Step16: Timeout for  read(VMIN, VTIME)

--- kilo.c
+++ kilo.c
@@ -17,9 +17,14 @@ void enableRawMode()
     atexit(disableRawMode);
 
     struct termios raw = orig_termios;
-    raw.c_iflag &= ~(ICRNL | IXON);
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
     raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+
+    // set a timeout for read, so read() returns if it doesn't get any input for a certain amount of time.
+    raw.c_cc[VMIN] = 0;  // set to 0, so that read returns as soon as there is any input to be read.
+    raw.c_cc[VTIME] = 1; // set 1/10 seconds, 100 milliseconds.
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -28,8 +33,9 @@ int main()
 {
     enableRawMode();
 
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
+    char c = '\0';
+    read(STDIN_FILENO, &c, 1);
+    while (1)
     {
         if (iscntrl(c))
         {
@@ -39,6 +45,8 @@ int main()
         {
             printf("%d (%c)\r\n", c, c);
         }
+        if (c == 'q')
+            break;
     }
     return 0;
 }
