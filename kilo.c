d356d5ff6887e5f10d600f012ce02a6a4ff2753b
forfd8960 <forfd8960@gmail.com>
2024-03-16 11:49:53 +0800

    Step25: Draw Tildes

--- kilo.c
+++ kilo.c
@@ -64,9 +64,23 @@ char editorReadKey(void)
 }
 
 /*** output ***/
+
+void editorDrawRows(void)
+{
+    int y;
+    for (y = 0; y < 24; y++)
+    {
+        write(STDOUT_FILENO, "~\r\n", 3);
+    }
+}
+
 void editorRefreshSceen(void)
 {
     write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
+    editorDrawRows();
+
     write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
