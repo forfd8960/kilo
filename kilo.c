4313e707ed9144219ed5742f1996c0e7aea63d93
forfd8960 <forfd8960@gmail.com>
2024-04-01 08:40:54 +0800

    Step 50: Editor Read PageUp and PageDown Key

--- kilo.c
+++ kilo.c
@@ -19,7 +19,9 @@ enum editorKey
     ARROW_LEFT = 1000,
     ARROW_RIGHT,
     ARROW_UP,
-    ARROW_DOWN
+    ARROW_DOWN,
+    PAGE_UP,
+    PAGE_DOWN,
 };
 
 /*** data ***/
@@ -95,16 +97,36 @@ int editorReadKey(void)
 
         if (seq[0] == '[')
         {
-            switch (seq[1])
+            if (seq[1] >= 0 && seq[1] <= 9)
             {
-            case 'A':
-                return ARROW_LEFT;
-            case 'B':
-                return ARROW_DOWN;
-            case 'C':
-                return ARROW_RIGHT;
-            case 'D':
-                return ARROW_UP;
+                if (read(STDIN_FILENO, &seq[2], 1) != 1)
+                    return '\x1b';
+
+                if (seq[2] == '~')
+                {
+                    switch (seq[1])
+                    {
+                    case '5': // page up: `\x1b[5~`
+                        return PAGE_UP;
+
+                    case '6': // page up: `\x1b[6~`
+                        return PAGE_DOWN;
+                    }
+                }
+            }
+            else
+            {
+                switch (seq[1])
+                {
+                case 'A':
+                    return ARROW_LEFT;
+                case 'B':
+                    return ARROW_DOWN;
+                case 'C':
+                    return ARROW_RIGHT;
+                case 'D':
+                    return ARROW_UP;
+                }
             }
         }
 
