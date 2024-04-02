934b4791f940d15bcb745ea34daa2c9951844d08
forfd8960 <forfd8960@gmail.com>
2024-04-02 08:56:40 +0800

    Step 51: Page Up and Page Down

--- kilo.c
+++ kilo.c
@@ -336,6 +336,16 @@ void editorProcessKeypress(void)
         exit(0);
         break;
 
+    case PAGE_UP:
+    case PAGE_DOWN:
+    {
+        int times = E.screenrows;
+        while (times--)
+        {
+            editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+        }
+    }
+
     case ARROW_UP:
     case ARROW_DOWN:
     case ARROW_LEFT:
